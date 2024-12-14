3e29dcd4602e68d601e0da3abe7c7d3e33599bc6
roman9234 <roman_lesovoy@mail.ru>
2024-12-14 13:13:07 +0300

    changed user validation

--- api/serializers.py
+++ api/serializers.py
@@ -4,11 +4,42 @@ from rest_framework import validators
 
 from api.models import ApiUser, Warehouse, Product, Request
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ApiUser
-        fields = "__all__"
-        extra_kwargs = {"id": {"read_only": True}}
+class UserSerializer(serializers.Serializer):
+    # Также добавим валидацию уникальности - username должен быть уникальным
+    username = serializers.CharField(max_length=128, validators=[
+        # Проверяем нет ли уже такого username используя встроенный в validators валидатор
+        validators.UniqueValidator(ApiUser.objects.all())
+    ])
+    # у serializers есть специальное поле для Email
+    email = serializers.EmailField(validators=[
+        validators.UniqueValidator(ApiUser.objects.all())
+    ])
+    # Тут лучше было бы настроить валидацию пароля
+    # write_only=True не позволяет отправить пароль в открытом виде клиенту после регистрации
+    password = serializers.CharField(min_length=6, max_length=20, write_only=True)
+
+    def update(self, instance, validated_data):
+        # username не обновляется
+        if email := validated_data.get("email"):
+            instance.email = email
+            instance.save(update_fields=["email"])
+
+        if password := validated_data.get("password"):
+            instance.set_password(password)
+            instance.save(update_fields=["password"])
+        return instance
+
+    def create(self, validated_data):
+        # Простая логика:
+        user = ApiUser.objects.create(
+            email=validated_data["email"],
+            username=validated_data["username"],
+        )
+        # Так как пароль хранится в зашифрованном виде, для его задания нужно использовать метод set_password
+        user.set_password(validated_data["password"])
+        # Сохраняем изменения. Пишем что обновили только одно поле, так как остальные были изменены при создании
+        user.save(update_fields=["password"])
+        return user
 
 class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
