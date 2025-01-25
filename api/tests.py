0bab8b1e28ec2864ebe84570443ac5e8f7d94355
roman9234 <roman_lesovoy@mail.ru>
2025-01-25 15:38:22 +0300

    added homepage test

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,17 @@
+# pylint: disable=missing-function-docstring
 """
-Неиспользуемый файл для тестов
+Файл для тестов
 """
 # from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+
+class HomepageTests(TestCase):
+    """
+    Тест проверки доступности главной страницы
+    """
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
